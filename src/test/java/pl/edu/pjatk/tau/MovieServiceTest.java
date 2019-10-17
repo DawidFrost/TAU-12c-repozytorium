2641ecc131583a316c5c2feb3c751550303842ce
DAvid <s15670@pjwstk.edu.pl>
2019-10-17 21:45:18 +0200

    add tests

--- src/test/java/pl/edu/pjatk/tau/MovieServiceTest.java
+++ src/test/java/pl/edu/pjatk/tau/MovieServiceTest.java
@@ -7,10 +7,12 @@ import org.junit.Test;
 import pl.edu.pjatk.tau.service.MovieService;
 import pl.edu.pjatk.tau.service.MovieServiceImpl;
 
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.Assert.*;
 
 public class MovieServiceTest {
     private MovieService service;
@@ -26,6 +28,21 @@ public class MovieServiceTest {
         movie_3 = new Movie("Name_3", "Type3", "Director3");
     }
 
+    @Test
+    public void delete() {
+
+        Integer id1 = service.create(movie_1);
+        Integer id2 = service.create(movie_2);
+
+
+        service.delete(id2);
+
+        assertThatExceptionOfType(NoSuchElementException.class)
+                .isThrownBy( () -> service.read(id1) );
+        
+        assertThat(service.read(id2)).isNull();
+        assertThat(service.readAll()).hasSize(1);
+    }
 
     @Test
     public void readAllTest() {
@@ -40,13 +57,24 @@ public class MovieServiceTest {
     }
 
     @Test
+    public void readMovie() {
+        service.create(movie_1);
+        Movie testMovie = service.read(0);
+
+        assertEquals(movie_1.getId(), testMovie.getId());
+        assertEquals(movie_1.getName(), testMovie.getName());
+        assertEquals(movie_1.getType(), testMovie.getType());
+        assertEquals(movie_1.getDirector(), testMovie.getDirector());
+    }
+
+    @Test
     public void updateWhenNotPresent() {
         assertThatExceptionOfType(NoSuchElementException.class)
                 .isThrownBy( () -> service.update(movie_1) );
     }
 
-    @Test
-    public void getAllWhenEmpty() {
+    @Test   
+    public void readAllWhenEmpty() {
         assertThat(service.readAll())
                 .isNotNull()
                 .isEmpty();
@@ -67,11 +95,23 @@ public class MovieServiceTest {
     }
 
     @Test
-     public void createTest(){
+    public void createTest(){
         service.create(movie_1);
+        service.create(movie_2);
 
+        assertNotEquals(movie_1,movie_2);
         assertThat(movie_1.getId()).isNotNull();
-        assertThat(movie_2.getId()).isNull();
+        assertThat(movie_3.getId()).isNull();
+    }
+
+    @Test
+    public void removeAllTest() {
+        service.create(movie_1);
+        service.create(movie_2);
+        service.removeAll();
+
+        assertEquals(0, service.readAll().size());
+
     }
 
     @After
@@ -80,4 +120,3 @@ public class MovieServiceTest {
     }
 
 }
-
